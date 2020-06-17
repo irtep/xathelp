988eb71f96a0bb9831099cee0f737eb28224f347
irtep <rasanen.petri@gmail.com>
2020-06-17 15:28:16 +0300

    start of app

--- public/functions.js
+++ public/functions.js
@@ -7,12 +7,12 @@ const newResponse = document.getElementById('newResponse');
 
 export function addNewEntry() {
   if (newQuestion.value !== '' && newResponse.value !== '') {
-    const newEntry = {question: newQuestion.value, response: newResponse.value}
+    const newEntry = {site: newQuestion.value, psw: newResponse.value}
     const pack = JSON.stringify(newEntry);
     const http = new XMLHttpRequest();
     const url = '/addNew';
     let params = 'MSG=' + pack;
-    
+
     http.open('POST', url, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     http.onreadystatechange = () => {
@@ -24,7 +24,7 @@ export function addNewEntry() {
   }
 }
 
-// this will be copy to clipboard mechanism:
+// this is copy to clipboard mechanism:
 // from my very early work, that i copied from somewhere from the internet...
 	export function copyToClipboardMsg(elem, msgElem) {  // copy message
     //console.log('got: ', elem, msgElem);
@@ -33,7 +33,7 @@ export function addNewEntry() {
 	    if (!succeed) {
 	      msg = "Copy not supported or blocked.  Press Ctrl+c to copy."
 	    } else {
-	      msg = "Kopioitu leikepöydälle. CONTROL+V voit liimata chattiin. Huom! kaikki vastaukset eivät käy suoraan vastauksiksi asiakkaille, joten lue ja muokkaa niitä aina!"
+	      msg = "Kopioitu leikepöydälle. CONTROL+V voit liimata."
 	    }
 	    if (typeof msgElem === "string") {
 	      msgElem = document.getElementById('infoScreen');
@@ -75,7 +75,7 @@ export function addNewEntry() {
 	    var succeed;
 	    try {
 	    	succeed = document.execCommand("copy");
-	    } 
+	    }
 	    catch(e) {
 	      succeed = false;
 	    }
@@ -86,7 +86,7 @@ export function addNewEntry() {
 	    if (isInput) {
 	      // restore prior selectio
 	      elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-	    } 
+	    }
 	    else {
 	      // clear temporary content
 	      target.textContent = "";
